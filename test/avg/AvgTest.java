bac3868c9ab7e059405858ec42cf58af53ba1e48
rjust <rjust.dev@gmail.com>
2022-04-29 16:30:31 -0700

    Transform the two tests into a parameterized unit test.

--- test/avg/AvgTest.java
+++ test/avg/AvgTest.java
@@ -2,26 +2,40 @@ package avg;
 
 import org.junit.Ignore;
 import org.junit.Test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
  * Test class for the Avg implementation.
  */
+@RunWith(Parameterized.class)
 public class AvgTest {
   // The delta up to which two doubles are considered equal
   private static double EPS = 1e-9;
 
-  @Test
-  public void test1() {
-    double actual = new Avg().avgAbs(new double[]{1, 2, 3});
-    double expected = 2.0;
-    assertEquals(expected, actual, EPS);
+  @Parameters
+  public static List<Object[]> data() {
+    return Arrays.asList(new Object[][] {
+        { new double[]{ 1,  2,  3}, 2},
+        { new double[]{-1, -2, -3}, 2},
+    });
   }
 
+  @Parameter(0)
+  public double[] numbers;
+  @Parameter(1)
+  public double expectedOutput;
+
   @Test
-  public void test2() {
-    double actual = new Avg().avgAbs(new double[]{-1, -2, -3});
-    double expected = 2.0;
-    assertEquals(expected, actual, EPS);
+  public void testAvgAbs() {
+    assertEquals(new Avg().avgAbs(numbers), expectedOutput, EPS);
   }
 }
